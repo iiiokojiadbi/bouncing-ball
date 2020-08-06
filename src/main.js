b72fed4709d2b92b60aade682708c48e47ef7c46
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2020-08-06 16:07:01 +0300

    feat: заставить шар прыгать

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
 import './index.css';
+import { btnStart, btnReser, btnReset } from './utils/constants';
+import Game from './components/Game';
+
+const game = new Game();
+
+btnStart.addEventListener('click', () => game.startGame());
+btnReset.addEventListener('click', () => game.resetGame());
