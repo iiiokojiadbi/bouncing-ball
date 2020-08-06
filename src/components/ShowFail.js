45afd3f3543062641a85d819c0a0472fc05177b8
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2020-08-06 22:32:39 +0300

    feat: добавить новый компонент ShowFail

---
+++ src/components/ShowFail.js
@@ -0,0 +1,13 @@
+export default class ShowFail {
+  constructor(showSelector) {
+    this._showEl = document.querySelector(showSelector);
+  }
+
+  showFail = () => {
+    this._showEl.classList.add('game__show_active');
+  };
+
+  hideFail = () => {
+    this._showEl.classList.remove('game__show_active');
+  };
+}
