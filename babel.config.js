0d7e61902c5ab48d7932c457d0ef74e8cfcf14ab
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2020-08-06 16:06:34 +0300

    feat: добавить модуль @babel/plugin-proposal-class-properties

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,18 @@
-const presets = [
-  [
-    '@babel/env',
-    {
-      // какой пресет использовать
-      targets: {
-        // какие версии браузеров поддерживать
-        edge: '17',
-        ie: '11',
-        firefox: '50',
-        chrome: '64',
-        safari: '11.1',
+module.exports = {
+  presets: [
+    [
+      '@babel/env',
+      {
+        targets: {
+          edge: '17',
+          ie: '11',
+          firefox: '50',
+          chrome: '64',
+          safari: '11.1',
+        },
+        useBuiltIns: 'entry',
       },
-
-      // использовать полифиллы для браузеров из свойства target
-      // по умолчанию babel использует поллифиллы библиотеки core-js
-      useBuiltIns: 'entry',
-    },
+    ],
   ],
-];
-
-module.exports = { presets };
+  plugins: ['@babel/plugin-proposal-class-properties'],
+};
