5a54e99f17fac0f31c117aaf6c6690f38968eacd
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2020-08-06 16:05:10 +0300

    feat: создать константы

---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+export const btnStart = document.querySelector('.btn_type_start');
+export const btnReset = document.querySelector('.btn_type_reset');
+
+export const ball = document.querySelector('.ball');
+export const barrier = document.querySelector('.barrier');
+
+export const SPACE = 'Space';
