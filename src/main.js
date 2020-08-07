bc7f67fbc571a7c1fddb78374ded515ef117ca99
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2020-08-07 18:46:46 +0300

    небольшие правки в коде

--- src/main.js
+++ src/main.js
@@ -8,18 +8,24 @@ const btnReset = document.querySelector(btnResetSelector);
 const game = new Game();
 
 function toogleBtnStatus(btn) {
+  if (btn.classList.contains('btn_is_active')) {
+    btn.setAttribute('disabled', 'disabled');
+  } else {
+    btn.removeAttribute('disabled');
+  }
   btn.classList.toggle('btn_is_disabled');
   btn.classList.toggle('btn_is_active');
 }
 
 btnStart.addEventListener('click', () => {
   toogleBtnStatus(btnStart);
-  btnStart.setAttribute('disabled', 'disabled');
+  toogleBtnStatus(btnReset);
+
   game.startGame();
 });
 btnReset.addEventListener('click', () => {
   toogleBtnStatus(btnStart);
-  btnStart.removeAttribute('disabled');
+  toogleBtnStatus(btnReset);
 
   game.resetGame();
 });
