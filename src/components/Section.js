d32c97bd28e8a0af8a31cba3fbd792c56433796b
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2020-08-07 18:47:11 +0300

    feat: добавить новый компонент Section

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,17 +1,34 @@
 export default class Section {
-  constructor({ items, rendered }, containerSelector) {
+  constructor({ items, rendered, setItem }, containerSelector) {
     this._renderedItems = items;
-    console.log(items);
     this._rendered = rendered;
+    this._setItem = setItem;
     this._container = document.querySelector(containerSelector);
     console.log(this._container);
   }
 
   renderItems = () => {
-    this._renderedItems.forEach((item) => {
-      console.log(item);
+    const item = this._renderedItems.pop();
+    this._rendered(item);
+    this._setItem(item);
+    this._intervalRender = setInterval(() => {
+      const item = this._renderedItems.pop();
+      this._container.querySelector('.barrier_is_move').remove();
       this._rendered(item);
+      this._setItem(item);
+    }, 3000);
+  };
+
+  resetItems = () => {
+    clearInterval(this._intervalRender);
+    this._renderedItems = null;
+    this._container.querySelectorAll('.barrier_is_move').forEach((elem) => {
+      elem.remove();
     });
+  };
+
+  setItems = (items) => {
+    this._renderedItems = items;
   };
 
   addItem = (element) => {
